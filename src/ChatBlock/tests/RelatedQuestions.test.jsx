6ce761d28a711d7ecb20b18939e618312810327e
Alin Voinea <contact@avoinea.com>
2026-03-11 00:12:30 +0200

    tests: Fix jest-dom import

--- src/ChatBlock/tests/RelatedQuestions.test.jsx
+++ src/ChatBlock/tests/RelatedQuestions.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
 import RelatedQuestions from '../components/RelatedQuestions';
 
 import { trackEvent } from '@eeacms/volto-matomo/utils';
--- src/ChatBlock/tests/useDeepCompareMemoize.test.js
+++ src/ChatBlock/tests/useDeepCompareMemoize.test.js
@@ -1,5 +1,5 @@
 import { renderHook } from '@testing-library/react-hooks';
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
 import { useDeepCompareMemoize } from '../hooks/useDeepCompareMemoize';
 
 describe('useDeepCompareMemoize', () => {
